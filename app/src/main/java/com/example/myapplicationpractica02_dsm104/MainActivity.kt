75c92df0c4d7545b6ad61142acf8f2f5c2fd4bef
RicardoMurciaa <RicardoMurciaa>
2024-07-12 00:18:43 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplicationpractica02_dsm104/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.myapplicationpractica02_dsm104
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        lifecycle.addObserver(MiLifeCycleObserver())
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show()
+    }
+
+    override  fun onStart() {
+        super.onStart()
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show()
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show()
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show()
+        super.onDestroy()
+    }
+}
